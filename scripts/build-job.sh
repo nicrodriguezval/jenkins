9d19dfad8e78b1d8bbc08ad6a88cd709d82830df
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2025-07-14 16:10:38 -0500

    Comment how to build a job with params

--- scripts/build-job.sh
+++ scripts/build-job.sh
@@ -7,3 +7,9 @@ JOB_NAME=$3
 curl \
 -u $USER:$API_KEY \
 -X POST http://localhost:8080/job/$JOB_NAME/build?delay=0sec
+
+# curl \
+# -u $USER:$API_KEY \
+# -X POST http://localhost:8080/job/$JOB_NAME/buildWithParameters?delay=0sec
+# -d parm1=value1 \
+# -d parm2=value2
